dc37bc4723872dc25714275efc1f522b41de75da
beto <alberto30120@gmail.com>
2018-07-22 18:59:57 -0400

    model search modify

--- FileSearch/src/com/fundation/search/model/ModelSearch.java
+++ FileSearch/src/com/fundation/search/model/ModelSearch.java
@@ -14,114 +14,71 @@
 package com.fundation.search.model;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.UserPrincipal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * This class ModelSearch can be FileResult, MultimediaResult and maybe SearchFolder.
- *
  * @author Yerel Hurtado - AT-[07].
+ * @author Erik Vargas - AT-[07].
  * @version 1.0.
  */
 public class ModelSearch {
     /**
      * this Array storage files find with specific arguments.
      */
-    private List<String> pathList = new ArrayList<>();
+    private List<AssetFile> pathList = new ArrayList<>();
 
     /**
      * @param directory patch to search files.
-     * @param nameFile is file to search.
-     * @param type extension to file.
-     * @param size file to search.
-     * @param hidden if file hidden true.
-     * @param owner of file create.
+     * @param nameFile  is file to search.
+     * @param type      extension to file.
+     * @param size      file to search.
+     * @param hidden    if file hidden true.
+     * @param owner     of file create.
      * @return List String name file.
      * @throws IOException file.
      * @throws IOException file.
      */
-    public List<String> searchPathName(String directory, String nameFile, String type, long size, boolean hidden, String owner) throws IOException {
+    public List<AssetFile> searchPathName(String directory, String nameFile, String type,
+                                          long size, boolean hidden, String owner) throws IOException {
+
 
         File files = new File(directory);
         File[] ficheros = files.listFiles();
 
-        FilenameFilter filter = (dir1, name) -> name.contains(nameFile);
-        File[] childrenFiles = files.listFiles(filter);
+        for (File fileIterate : ficheros) {
 
-        for (File fileIterate : Objects.requireNonNull(childrenFiles)) {
-            if (fileIterate.isHidden() == hidden) {
-                if (type != null) {
-                    if (fileIterate.getName().toLowerCase().endsWith(type)) {
-                        if (size != 0) {
-                            if (fileIterate.length() == size) {
-                                if (owner != null) {
-                                    Path filePath = Paths.get(fileIterate.getPath());
-                                    FileOwnerAttributeView ownerInfo = Files.getFileAttributeView(filePath, FileOwnerAttributeView.class);
-                                    UserPrincipal fileOwner = ownerInfo.getOwner();
-                                    if (fileOwner.getName().equalsIgnoreCase(owner)) {
-                                        pathList.add(fileIterate.getName());
-                                    }
-                                } else {
-                                    pathList.add(fileIterate.getName());
-                                }
-                            }
-                        } else {
-                            if (owner != null) {
-                                Path filePath = Paths.get(fileIterate.getPath());
-                                FileOwnerAttributeView ownerInfo = Files.getFileAttributeView(filePath, FileOwnerAttributeView.class);
-                                UserPrincipal fileOwner = ownerInfo.getOwner();
-                                if (fileOwner.getName().equalsIgnoreCase(owner)) {
-                                    pathList.add(fileIterate.getName());
-                                }
-                            } else {
-                                pathList.add(fileIterate.getName());
-                            }
-                        }
-                    }
-                } else {
-                    if (size != 0) {
-                        if (fileIterate.length() == size) {
-                            if (owner != null) {
-                                Path filePath = Paths.get(fileIterate.getPath());
-                                FileOwnerAttributeView ownerInfo = Files.getFileAttributeView(filePath, FileOwnerAttributeView.class);
-                                UserPrincipal fileOwner = ownerInfo.getOwner();
-                                if (fileOwner.getName().equalsIgnoreCase(owner)) {
-                                    pathList.add(fileIterate.getName());
-                                }
-                            } else {
-                                pathList.add(fileIterate.getName());
-                            }
-                        }
-                    } else {
-                        if (owner != null) {
-                            Path filePath = Paths.get(fileIterate.getPath());
-                            FileOwnerAttributeView ownerInfo = Files.getFileAttributeView(filePath, FileOwnerAttributeView.class);
-                            UserPrincipal fileOwner = ownerInfo.getOwner();
-                            if (fileOwner.getName().equalsIgnoreCase(owner)) {
-                                pathList.add(fileIterate.getName());
-                            }
-                        } else {
-                            pathList.add(fileIterate.getName());
-                        }
-                    }
+            if (fileIterate.isDirectory()) {
+                searchPathName(fileIterate.getPath(), nameFile, type, size, hidden, owner);
+            } else {
+                Path filePath = Paths.get(fileIterate.getPath());
+                UserPrincipal ownerFile = Files.getOwner(filePath, LinkOption.NOFOLLOW_LINKS);
+                if (fileIterate.isHidden() != hidden) {
+                    continue;
                 }
-            }
-        }
-        assert ficheros != null;
-        for (File fichero : ficheros) {
-            if (fichero.isDirectory()) {
-                searchPathName(fichero.getPath(), nameFile, type, size, hidden, owner);
+                if (type != null && !fileIterate.getName().toLowerCase().endsWith(type)) {
+                    continue;
+                }
+                if (size > 0 && fileIterate.length() != size) {
+                    continue;
+                }
+                if (nameFile != null && !fileIterate.getName().contains(nameFile)) {
+                    continue;
+                }
+                if (owner != null && !ownerFile.getName().equals(owner)) {
+                    continue;
+                }
+
+                pathList.add(new AssetFile(fileIterate.getAbsolutePath(), fileIterate.getName(), fileIterate.length(), type, ownerFile.getName()));
             }
         }
         return pathList;
     }
 }
-
